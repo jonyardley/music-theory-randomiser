c22d40f79fb086ff5111c52792e39ce36fa7d63e
Jon Yardley <jon.yardley@red-badger.com>
2022-01-03 13:14:50 +0000

    Add button to generate a new randome key

--- components/KeysTab.tsx
+++ components/KeysTab.tsx
@@ -1,6 +1,7 @@
-import { CardContent, Card, Typography } from '@mui/material';
+import { CardContent, Card, Typography, Button } from '@mui/material';
 import { Accidentals, Notes } from './Data';
 import { sample } from 'lodash';
+import { useState } from 'react';
 
 const RandomKey = () => {
   const tonic = sample(Notes);
@@ -8,6 +9,14 @@ const RandomKey = () => {
   return `${tonic}${accidental}`;
 };
 
-const KeysTab = () => <Typography variant="h1">{RandomKey()}</Typography>;
+const KeysTab = () => {
+  const [randomKey, setRandomKey] = useState(RandomKey());
+  return [
+    <Typography variant="h1">{randomKey}</Typography>,
+    <Button variant="contained" onClick={() => setRandomKey(RandomKey())}>
+      New key
+    </Button>
+  ];
+};
 
 export default KeysTab;
