3af98c321ab4f46bf312fbc225bae99e54955c9e
Jon Yardley <jon.yardley@red-badger.com>
2022-01-03 12:39:50 +0000

    Added data for keys and intervals

---
+++ components/Data.ts
@@ -0,0 +1,12 @@
+export const Notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+export const Accidentals = ['', '#', 'b'];
+export const Intervals = {
+  second: ['major', 'minor', 'augmented'],
+  third: ['major', 'minor'],
+  fourth: ['diminished', 'perfect', 'augmented'],
+  fifth: ['diminished', 'perfect', 'augmented'],
+  sixth: ['major', 'minor'],
+  seventh: ['diminished', 'major', 'minor'],
+  octave: ['perfect']
+};
+// export const ChordQuality = ['major', 'minor', 'diminished'];
